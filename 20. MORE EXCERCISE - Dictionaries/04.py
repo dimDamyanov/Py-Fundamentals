dabebf6f4aa71e579d0eb1809b5c40ccdeb7eb7f
dimDamyanov <dim.damianov@gmail.com>
2020-12-08 16:16:51 +0200

    Add files

--- 20. MORE EXCERCISE - Dictionaries/04.py
+++ 20. MORE EXCERCISE - Dictionaries/04.py
@@ -1,1 +1,25 @@
-# TODO: https://judge.softuni.bg/Contests/Practice/Index/1738#3
+dwarfs = {}
+line = input()
+
+while line != 'Once upon a time':
+    name, color, phy = line.split(' <:> ')
+    phy = int(phy)
+    if color in dwarfs:
+        if name in dwarfs[color]:
+            if phy > dwarfs[color][name]:
+                dwarfs[color][name] = phy
+        else:
+            dwarfs[color][name] = phy
+    else:
+        dwarfs[color] = {name: phy}
+    line = input()
+
+order = []
+for color, d in dwarfs.items():
+    n = len(d)
+    for name, phy in d.items():
+        order.append((name, color, phy, n))
+
+order.sort(key=lambda x: (-x[2], -x[3]))
+for dwarf in order:
+    print(f'({dwarf[1]}) {dwarf[0]} <-> {dwarf[2]}')
